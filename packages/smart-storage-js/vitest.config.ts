501017d17f706a7231aa17af23fa46e5c81b63ec
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-04-12 19:12:58 +0200

    feat: :wrench: use import aliases

--- packages/smart-storage-js/vitest.config.ts
+++ packages/smart-storage-js/vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@connector": path.resolve(__dirname, "src/connector"),
+      "@warehouse": path.resolve(__dirname, "src/warehouse"),
+    },
+  },
   test: {
     exclude: [],
   },
