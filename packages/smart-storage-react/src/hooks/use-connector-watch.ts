ac1bfb42170a228c021c0b7ed92e24e73be56049
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-05-05 20:12:49 +0200

    feat: :sparkles: add onChange callback support to useConnectorWatch and implement tests

--- packages/smart-storage-react/src/hooks/use-connector-watch.ts
+++ packages/smart-storage-react/src/hooks/use-connector-watch.ts
@@ -4,10 +4,15 @@ import type {
 } from "@themineway/smart-storage-js";
 import { useEffect, useState } from "react";
 
+type Options<V extends object> = {
+  schema?: SmartStorageSchema<V>;
+  onChange?: (value: V | null) => void;
+};
+
 export const useConnectorWatch = <V extends object>(
   connector: AConnector,
   key: string,
-  schema?: SmartStorageSchema<V>
+  { schema, onChange }: Options<V> = {}
 ) => {
   const [value, setValue] = useState<V | null>(connector.get(key, schema));
 
@@ -16,6 +21,7 @@ export const useConnectorWatch = <V extends object>(
       if (changedKey === key) {
         const newValue = connector.get<V>(key, schema);
         setValue(newValue);
+        onChange?.(newValue);
       }
     });
     return () => unsubscribe();
