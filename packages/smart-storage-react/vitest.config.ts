c85e34b123a1eaa73981372c1c7b544f2bd0d1ac
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-05-05 20:46:47 +0200

    fix: :bug: update package versions and dependencies for smart-storage packages

--- packages/smart-storage-react/vitest.config.ts
+++ packages/smart-storage-react/vitest.config.ts
@@ -5,9 +5,23 @@ export default defineConfig({
   resolve: {
     alias: {
       "@hooks": path.resolve(__dirname, "src/hooks/index.ts"),
+      "@themineway/smart-storage-js": path.resolve(
+        __dirname,
+        "../smart-storage-js/src/index.ts"
+      ),
     },
   },
   test: {
     exclude: ["node_modules", "dist"],
+  },
+  build: {
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React", // Global variable for React in UMD builds
+        },
+      },
+    },
   },
 });
