8fad7411eae839b8112cf53c3a43d4485db07648
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-05-18 23:56:17 +0200

    Revert "feat: use zod object"
    This reverts commit cd5b6a9d601b7fbb11dc401a2624ba4c3a0d294b.

--- packages/smart-storage-js/src/types/schema.type.ts
+++ packages/smart-storage-js/src/types/schema.type.ts
@@ -1,7 +1,5 @@
+import type { ZodSchema } from "zod";
 import type { Schema as YupSchema } from "yup";
-import { ZodObject, ZodRawShape } from "zod";
 
-type ZodSchema = ZodObject<ZodRawShape>;
-
-export type SmartStorageSchema<V> = ZodSchema | YupSchema<V>;
+export type SmartStorageSchema<V> = ZodSchema<V> | YupSchema<V>;
 export type EmptySmartStorageSchema = ZodSchema | YupSchema;
